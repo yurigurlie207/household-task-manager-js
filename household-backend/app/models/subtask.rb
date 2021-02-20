c26f93890da3c6161b0c3940fb5b9017315857c8
yurigurlie207 <adrienne.yao@gmail.com>
2021-02-20 15:22:07 -0500

    Trimmed some validations in models, for ease of seeding

--- household-backend/app/models/subtask.rb
+++ household-backend/app/models/subtask.rb
@@ -7,10 +7,6 @@ class Subtask < ApplicationRecord
   has_many :users, :through => :user_tasks
 
   validates :title, presence: true
-  validates :notes, length: { maximum: 500 }
 
-  # validates_with SubtaskusersValidator - decided not to build this, because user_ids isn't really part of a subtask record
-  #also, functionally, there doesn't need to be a user assigned to a task or subtask, as it's just a decision 
-  # the user hasn't made yet. better to just highlight it maybe with stylesheets if no users assigned
 
 end
--- household-backend/app/models/task.rb
+++ household-backend/app/models/task.rb
@@ -4,15 +4,6 @@ class Task < ApplicationRecord
     # accepts_nested_attributes_for :subtasks 
 
     validates :title, presence: true
-    validates :notes, length: { maximum: 500 }
+   
  
-    validates :deadline, date: { on_or_after: Proc.new { Time.now } }
-
-    def subtask_attributes=(subtask_attributes)
-            subtask = self.subtasks.build(title: subtask_attributes[:subtasks][:title])
-            subtask.complete = false
-            subtask.save 
-            subtask.user_ids = subtask_attributes[:subtasks][:user_ids] 
-    end
-
 end
