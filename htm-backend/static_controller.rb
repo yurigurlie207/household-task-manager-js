d471757a819dd29a7c8f6f5a9e5515c7de708426
yurigurlie207 <adrienne.yao@gmail.com>
2021-02-20 11:36:50 -0500

    Deleted the extra controllers that were sitting in the backend folder

--- htm-backend/static_controller.rb
+++
@@ -1,9 +0,0 @@
-class StaticController < ApplicationController
-    def home
-        if session[:user_id] then
-             redirect_to user_path(User.find(session[:user_id]))
-        else
-            render :home
-        end
-    end
-end
