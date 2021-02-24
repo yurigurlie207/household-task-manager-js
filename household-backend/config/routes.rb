415a351f4334e820bdc19ebfea917300b74842a0
yurigurlie207 <adrienne.yao@gmail.com>
2021-02-23 21:41:41 -0500

    Added session and user routes, and session and static views

--- household-backend/config/routes.rb
+++ household-backend/config/routes.rb
@@ -6,4 +6,13 @@ Rails.application.routes.draw do
   resources :user_tasks, :only => [:index]
   resources :subtasks, :only => [:index]
 
+  root 'static#home', as: 'home'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+
+  resources :user
+
 end
