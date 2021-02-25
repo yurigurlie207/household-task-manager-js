ac085fdb0eaf04a292ed77f87474d5bc70c6f93a
yurigurlie207 <adrienne.yao@gmail.com>
2021-02-24 22:04:15 -0500

    Added Static controller file

---
+++ household-backend/app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+
+class StaticController < ApplicationController
+    def home
+        if session[:user_id] then
+             redirect_to user_path(User.find(session[:user_id]))
+        else
+            render :home
+        end
+    end
+end
