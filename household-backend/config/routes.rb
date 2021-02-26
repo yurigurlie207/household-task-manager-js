abad6ec88e9969264f926c7d631899d9a3ae26ab
yurigurlie207 <adrienne.yao@gmail.com>
2021-02-26 16:28:38 -0500

    Updated the public index page

--- household-backend/config/routes.rb
+++ household-backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/test', to: 'application#test'
-  get '*other', to: 'static#index'
+  # get '*other', to: 'static#index'
 
   resources :user_tasks, :only => [:index]
   resources :subtasks, :only => [:index]
