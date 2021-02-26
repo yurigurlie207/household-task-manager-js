e701ac883f4a5563af7af76252ea47dc773345e7
yurigurlie207 <adrienne.yao@gmail.com>
2021-02-26 10:09:10 -0500

    Added an index page in public folder since my static page wasn't working, and updated routes.rb accordingly

--- household-backend/app/controllers/static_controller.rb
+++ household-backend/app/controllers/static_controller.rb
@@ -1,10 +1,11 @@
+require 'rails/application_controller'
 
 class StaticController < ApplicationController
     def home
         if session[:user_id] then
              redirect_to user_path(User.find(session[:user_id]))
         else
-            render :home
+            render file: Rails.root.join('public', 'index.html')
         end
     end
 end
--- household-backend/config/routes.rb
+++ household-backend/config/routes.rb
@@ -2,17 +2,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/test', to: 'application#test'
+  get '*other', to: 'static#index'
 
   resources :user_tasks, :only => [:index]
   resources :subtasks, :only => [:index]
+  # resources :tasks, :only => [:index]
 
-  root 'static#home', as: 'home'
+  # root 'static#home', as: 'home'
   
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 
-  resources :user
+  # resources :user
 
 end
