db0226f052eb1262cb9318884049e0c95d08d245
yurigurlie207 <adrienne.yao@gmail.com>
2021-02-26 16:19:23 -0500

    Deleted out the sessions and static and route paths for my previous intention of still being able to sign into my API

--- household-backend/config/routes.rb
+++ household-backend/config/routes.rb
@@ -10,11 +10,6 @@ Rails.application.routes.draw do
 
   # root 'static#home', as: 'home'
   
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy'
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-
   # resources :user
 
 end
