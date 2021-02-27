8448b3a21248922566190b36dd18bd90a1e4b073
yurigurlie207 <adrienne.yao@gmail.com>
2021-02-27 09:24:33 -0500

    Deleted the views, since I don't need them for this project

--- household-backend/config/routes.rb
+++ household-backend/config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/test', to: 'application#test'
-  # get '*other', to: 'static#index'
-
   resources :user_tasks, :only => [:index]
   resources :subtasks, :only => [:index]
   resources :users,  :only => [:index]
-  # resources :tasks, :only => [:index]
 
-  # root 'static#home', as: 'home'
-  
-  # resources :user
 
 end
