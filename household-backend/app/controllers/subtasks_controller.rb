c2a74df1509d338691ca9cc9cfffebeed2ddb00b
yurigurlie207 <adrienne.yao@gmail.com>
2021-02-27 11:01:06 -0500

    Uncommented rack-cors gem to make sure the fetch works

--- household-backend/app/controllers/subtasks_controller.rb
+++ household-backend/app/controllers/subtasks_controller.rb
@@ -6,12 +6,3 @@ class SubtasksController < ApplicationController
 end
 
 
-# {"data":
-# [{"id":"7","type":"subtask","attributes":{"title":"cook breakfast","estimated_duration":"20 minutes"}},
-# {"id":"7","type":"subtask","attributes":{"title":"cook breakfast","estimated_duration":"20 minutes"}},
-# {"id":"7","type":"subtask","attributes":{"title":"cook breakfast","estimated_duration":"20 minutes"}},
-# {"id":"8","type":"subtask","attributes":{"title":"vaccuum bedroom","estimated_duration":"10 minutes"}},
-# {"id":"9","type":"subtask","attributes":{"title":"grocery shop","estimated_duration":"90 minutes"}},
-# {"id":"10","type":"subtask","attributes":{"title":"power wash house","estimated_duration":"90 minutes"}},
-# {"id":"11","type":"subtask","attributes":{"title":"mow lawn","estimated_duration":"30 minutes"}},
-# {"id":"12","type":"subtask","attributes":{"title":"cook dinner","estimated_duration":"45 minutes"}}]}
